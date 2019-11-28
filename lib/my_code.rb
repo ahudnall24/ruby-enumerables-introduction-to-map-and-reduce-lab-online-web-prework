3978f81240308c5fa73d2f9c4636a34af0ed180d
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-28 08:11:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,13 @@
-map_to_negativize(source_array)
+map_to_negativize(-1[1,2,3])
+    =>[-1,-2,-3]
+
+map_to_no_change(1[1,2,3])
+  =>[1,2,3]
 
 
-map_to_no_change(source_array)
 
 
-
-
-map_to_double(source_array)
+map_to_double(2_array)
 
 
 
